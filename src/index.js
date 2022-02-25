eb810f6acd39f5cdd0fb7dcd74b69c3072c1f12e
NicMortelliti <npmortelliti@gmail.com>
2022-02-25 15:57:38 -0800

    Implemented: Set color of task text based on priority selected.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
 
     // Call function, passing in text field value as parameter
-    buildToDo(document.getElementById("new-task-description").value);
+    let taskValue = document.getElementById("new-task-description").value;
+    let level = document.getElementById("prioritySelect").value;
+
+    buildToDo(taskValue, level);
 
     // Revert text field to placeholder value
     document.getElementById("new-task-description").value = "";
@@ -13,25 +16,28 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Function to append new tasks to list
-function buildToDo(todo) {
+function buildToDo(todo, level) {
   // Set variable to create new paragraph element
   let p = document.createElement("p");
 
-  // Set variable to create new delete button
+  // Set variable to create delete button
   let btn = document.createElement("button");
   btn.textContent = "X";
 
-  // Listen for click on delete button
-  btn.addEventListener("click", handleDelete);
-
   // Set paragraph text to todo's value
-  p.textContent = `${todo} `;
+  p.textContent = `${todo}  `;
+
+  // Set paragraph text color based on priority level
+  p.style.color = priority[level][0];
 
   // Append button to the p element
   p.appendChild(btn);
 
-  // Append this new paragraph to tasks ul
+  // Append this new line to tasks ul
   document.querySelector("#tasks").appendChild(p);
+
+  // Listen for click on delete button
+  btn.addEventListener("click", handleDelete);
 }
 
 // Function to delete task from list
@@ -40,23 +46,37 @@ function handleDelete(e) {
 }
 
 // Function to build generic dropdown selection
-function buildSelect(choices) {
+function buildSelect(choices, id) {
   // Define new selection element
   const sel = document.createElement("select");
 
+  // Traverse through choices list and add it to
+  // selection dropdown
   choices.forEach(e => {
+    // Create new option element
     let level = document.createElement("option");
+
+    // Apply text content from choices list
+    // to this selection
     level.textContent = e;
+
+    // Finally, append this selection to
+    // the dropdown selection menu
     sel.appendChild(level);
   });
 
-  // Return newly build dropdown
+  // Add an id attribute to this menu so we
+  // can easily target it later
+  sel.setAttribute("id", id);
+
+  // Return newly built dropdown
   return sel;
 }
 
 // Define priority levels and call buildSelect function
-const priority = ["High", "Medium", "Low"];
-const prioritySelect = buildSelect(priority);
+//const priority = ["High", "Medium", "Low"];
+const priority = { High: ["red", 1], Medium: ["orange", 2], Low: ["Black", 3] };
+const prioritySelect = buildSelect(Object.keys(priority), "prioritySelect");
 
 // Insert a dropdown selection for priority classification
 let refNode = document.getElementById("new-task-description");
